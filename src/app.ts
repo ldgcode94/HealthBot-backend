7ceddaac6ca43d4e531c73052c0b365466569721
ldgcode94 <ldgcode@gmail.com>
2025-07-18 09:53:44 -0300

    IA service applied

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import healthRoutes from "./modules/health/infrastructure/http/routes/health.routes";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
+dotenv.config();
 
 const swaggerDocument = YAML.load("./src/docs/openapi.yaml");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
--- src/modules/health/infrastructure/services/GPTService.ts
+++ src/modules/health/infrastructure/services/GPTService.ts
@@ -1,9 +1,36 @@
 import { injectable } from "tsyringe";
+import axios from "axios";
 import { IGPTService } from "./IGPTService";
 
 @injectable()
 export class GPTService implements IGPTService {
   async getAnswer(question: string): Promise<string> {
-    return `This is a dummy answer for: "${question}"`;
+    const response = await axios.post(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        model: "deepseek/deepseek-chat",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a helpful and concise medical assistant. Answer only general medical questions and do not provide diagnoses or prescriptions. you should have the capacity to identify not medical questions and avoid answering them.",
+          },
+          {
+            role: "user",
+            content: question,
+          },
+        ],
+        temperature: 0.7,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const answer = response.data.choices[0].message.content;
+    return answer;
   }
 }
